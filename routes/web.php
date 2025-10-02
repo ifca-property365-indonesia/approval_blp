8c5ad3af09682ae754a0a65446b959828a6d92a3
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-10-02 11:58:10 +0700

    INDEX MOVE

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return 'Laravel root OK';
+    return view('welcome');
+    // return 'Laravel root OK';
 });
