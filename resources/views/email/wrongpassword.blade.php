ad930ef4e866f08bbbead36f99858dcc7fd5513b
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-10-13 11:32:52 +0700

    FUPB

--- resources/views/email/wrongpassword.blade.php
+++ resources/views/email/wrongpassword.blade.php
@@ -26,9 +26,7 @@
                         <tbody>
                             <tr>
                                 <td style="text-align: center; padding-bottom:25px">
-                                    <img src="{{ asset('public/images/email_header.png') }}" alt="logo"
-                                        width="120" height="56"
-                                        style="display:block; border:0; outline:none; text-decoration:none;">
+                                    <img src="{{ url('public/images/email_header.png') }}" alt="logo" height="56px" style="display:block; border:0; outline:none; text-decoration:none;">
                                         <p style="font-size: 16px; color: #026735; padding-top: 0px;">PT BANGUN LAKSANA PERSADA</p>
                                 </td>
                             </tr>
