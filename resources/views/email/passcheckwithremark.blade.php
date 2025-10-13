cf8d1c2f9c3996e987970c64674bd62625420273
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-10-13 12:15:53 +0700

    FUPB

--- resources/views/email/passcheckwithremark.blade.php
+++ resources/views/email/passcheckwithremark.blade.php
@@ -63,69 +63,7 @@
           </tr>
 
           <!-- Content -->
-          <tr>
-            <td class="content" style="background-color:#e0e0e0; padding:30px; color:#000000; font-size:14px; line-height:22px;">
-              <h5 style="font-size:20px; font-weight:400; margin:0 0 15px;">Dear {{ $dataArray['user_name'] }},</h5>
-              <p style="margin:0 0 15px;">Tolong berikan persetujuan untuk Form Penawaran Harga dengan detail :</p>
-
-              <!-- Detail Table -->
-              <table role="presentation" cellpadding="4" cellspacing="0" border="0" width="100%" style="font-size:14px; color:#000000;">
-                <tr><td width="40%">Nomor Dokumen</td><td width="2%">:</td><td>{{ $dataArray['doc_no'] }}</td></tr>
-                <tr><td>Nama PT</td><td>:</td><td>{{ $dataArray['entity_name'] }}</td></tr>
-                <tr><td>No. Alas Hak</td><td>:</td><td>{{ $dataArray['no_alas_hak'] }}</td></tr>
-                <tr><td>Nama Alas Hak</td><td>:</td><td>{{ $dataArray['name_alas_hak'] }}</td></tr>
-                <tr><td>Luas Alas Hak</td><td>:</td><td>{{ $dataArray['luas_alas_hak'] }}</td></tr>
-                <tr><td>Nama Pemilik</td><td>:</td><td>{{ $dataArray['name_owner'] }}</td></tr>
-                <tr><td>Tanggal FPH</td><td>:</td><td>{{ $dataArray['transaction_date'] }}</td></tr>
-                <tr><td>Harga yang disepakati</td><td>:</td><td align="right">Rp. {{ $dataArray['total_amt'] }}</td></tr>
-                <tr><td>Uang Tanda Jadi</td><td>:</td><td align="right">Rp. {{ $dataArray['book_amt'] }}</td></tr>
-              </table>
-
-              <!-- Attachments -->
-              @php $hasAttachment = false; @endphp
-              @foreach($dataArray['url_link'] as $key => $url_link)
-                @if($url_link && $dataArray['file_name'][$key] && $url_link != 'EMPTY' && $dataArray['file_name'][$key] != 'EMPTY')
-                  @if(!$hasAttachment)
-                    @php $hasAttachment = true; @endphp
-                    <p style="margin:20px 0 10px;">To view detail transaction, please click the link below:</p>
-                  @endif
-                  <a href="{{ $url_link }}" target="_blank" style="color:#026735; text-decoration:none;">{{ $dataArray['file_name'][$key] }}</a><br>
-                @endif
-              @endforeach
-
-              <!-- Buttons -->
-              <div style="text-align: center; margin: 20px 0;">
-                <a href="{{ url('api') }}/fph/A/{{ $encryptedData }}" class="button" style="display:inline-block; font-size:13px; font-weight:600; text-transform:uppercase; text-decoration:none; background-color:#1ee0ac; color:#ffffff; padding:10px 30px; border-radius:3px;">Approve</a>
-                <a href="{{ url('api') }}/fph/R/{{ $encryptedData }}" class="button" style="display:inline-block; font-size:13px; font-weight:600; text-transform:uppercase; text-decoration:none; background-color:#f4bd0e; color:#ffffff; padding:10px 30px; border-radius:3px;">Revise</a>
-                <a href="{{ url('api') }}/fph/C/{{ $encryptedData }}" class="button" style="display:inline-block; font-size:13px; font-weight:600; text-transform:uppercase; text-decoration:none; background-color:#e85347; color:#ffffff; padding:10px 30px; border-radius:3px;">Reject</a>
-              </div>
-
-              <p style="margin:15px 0;">In case you need some clarification, kindly approach:<br>
-                <a href="mailto:{{ $dataArray['clarify_email'] }}" style="color:#026735;">{{ $dataArray['clarify_user'] }}</a>
-              </p>
-
-              <p style="margin:15px 0;">
-                <b>Thank you,</b><br>
-                <a href="mailto:{{ $dataArray['sender_addr'] }}" style="color:#026735;">{{ $dataArray['sender_name'] }}</a>
-              </p>
-
-              <!-- Approval List -->
-              @php $hasApproval = false; $counter = 0; @endphp
-              @foreach($dataArray['approve_list'] as $key => $approve_list)
-                @if($approve_list && $approve_list != 'EMPTY')
-                  @if(!$hasApproval)
-                    @php $hasApproval = true; @endphp
-                    <p style="margin:15px 0;">This request approval has been approved by:<br>
-                  @endif
-                  {{ ++$counter }}. {{ $approve_list }}<br>
-                @endif
-              @endforeach
-              @if($hasApproval)</p>@endif
-
-              <p style="margin:15px 0;"><b>Please do not reply, this is an automated email.</b></p>
-
-            </td>
-          </tr>
+          
 
           <!-- Footer -->
           <tr>
